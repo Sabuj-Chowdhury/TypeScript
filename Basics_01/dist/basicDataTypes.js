8c812844b6c49d81c76224dab4b7f555f6cd4c77
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-05-01 00:08:43 +0530

    how to declare basic data types in TS

---
+++ Basics_01/dist/basicDataTypes.js
@@ -0,0 +1,19 @@
+"use strict";
+// string data type
+let city = "Dhaka";
+// number data type
+let randomNum = 89;
+// boolean data type
+let isOkay = true;
+// null data type
+let akbuum = null;
+// undefined data type
+let bakum = undefined;
+// any data type *** NOT recommended
+let anyVariable;
+// Array
+let fruits = ["mango", "orange"];
+let res = [11, 33, 45, 89.98];
+// tupple is ordered Array
+let eligible = ["max", 90];
+let student = ["max", 90, false];
---
+++ Basics_01/src/basicDataTypes.ts
@@ -0,0 +1,21 @@
+// string data type
+let city: string = "Dhaka";
+// number data type
+let randomNum: number = 89;
+// boolean data type
+let isOkay: boolean = true;
+// null data type
+let akbuum: null = null;
+// undefined data type
+let bakum: undefined = undefined;
+
+// any data type *** NOT recommended
+let anyVariable;
+
+// Array
+let fruits: string[] = ["mango", "orange"];
+let res: number[] = [11, 33, 45, 89.98];
+
+// tupple is ordered Array
+let eligible: [string, number] = ["max", 90];
+let student: [string, number, boolean] = ["max", 90, false];
