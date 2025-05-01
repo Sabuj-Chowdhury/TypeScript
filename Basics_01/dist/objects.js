243524d0d0d659c8c2a15666ccbb36423c017c9a
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-05-01 13:45:44 +0530

    how to declare objects in typeScript

---
+++ Basics_01/dist/objects.js
@@ -0,0 +1,9 @@
+"use strict";
+let studentData = {
+    firstName: "sabuj",
+    // lastName:"chowdhury",
+    age: 30,
+    cgpa: 3.7,
+    university: "Burdwan",
+};
+// objects declaration in TS
---
+++ Basics_01/src/objects.ts
@@ -0,0 +1,15 @@
+let studentData: {
+  firstName: string;
+  lastName?: string; //optional type ?
+  age: number;
+  cgpa: number;
+  university: "Burdwan";
+} = {
+  firstName: "sabuj",
+  // lastName:"chowdhury",
+  age: 30,
+  cgpa: 3.7,
+  university: "Burdwan",
+};
+
+// objects declaration in TS
